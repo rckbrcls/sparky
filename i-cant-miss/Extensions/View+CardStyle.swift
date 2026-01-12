635f6766a9fce252e5529c042be395634049d07f
brcls <petrick_barcelos@hotmail.com>
2026-01-12 19:50:09 -0300

    add assets

--- i-cant-miss/Extensions/View+CardStyle.swift
+++ i-cant-miss/Extensions/View+CardStyle.swift
@@ -5,12 +5,12 @@ extension View {
         self
             .background(
                 RoundedRectangle(cornerRadius: 12)
-                    .fill(Color(red: 16/255, green: 16/255, blue: 16/255))
+                    .fill(Color("ElementBackground"))
                     .shadow(color: .black.opacity(0.05), radius: 6, x: 0, y: 3)
             )
             .overlay(
                 RoundedRectangle(cornerRadius: 12)
-                    .stroke(Color(red: 20/255, green: 20/255, blue: 20/255), lineWidth: 2)
+                    .stroke(Color("ElementBorder"), lineWidth: 2)
             )
             .contentShape(Rectangle())
     }
